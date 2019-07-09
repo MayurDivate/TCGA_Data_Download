ac5ee543833d1bacd4f8ab14a0630d5f0a8c5981
Mayur Divate <mayur.divate91@gmail.com>
2019-07-09 18:58:37 +1000

    json to df info

--- TCGA_Download/rnaseq.py
+++ TCGA_Download/rnaseq.py
@@ -2,8 +2,23 @@ import json as js
 
 
 class RNAseq:
-    default_fields = ['file_name','cases.primary_site', 'cases.disease_type']
-    default_expand = 'cases,cases.samples,cases.project,cases.project.program'
+    default_fields = ['file_name',
+                      'cases.primary_site',
+                      'cases.disease_type',
+                      'cases.diagnoses.tissue_or_organ_of_origin',
+                      'cases.diagnoses.tumor_stage',
+                      'cases.diagnoses.primary_diagnosis',
+                      'cases.project.dbgap_accession_number',
+                      'cases.project.program.name',
+                      'cases.samples.sample_type',
+                      'cases.samples.tissue_type',
+                      'cases.samples.sample_id',
+                      'cases.samples.composition',
+                      'cases.demographic.gender',
+                      'cases.demographic.ethnicity'
+                      ]
+
+    # default_expand = 'cases.samples,cases.project,cases.project.program'
 
     def __init__(self, fields=None, expand=None):
 
@@ -12,20 +27,11 @@ class RNAseq:
         else:
             self.fields = fields
 
-        if expand is None:
-            self.expand = self.default_expand
-        else:
-            self.expand = expand
-
+        self.expand = expand
         self.filters = self.get_filters()
         self.params = self.get_params()
 
-
-
     def get_filters(self):
-
-        # data_category_filter = self.get_filter("=", "data_category", "Transcriptome Profiling")
-
         filters = {
             "op": "and",
             "content": [
@@ -60,8 +66,92 @@ class RNAseq:
             'format': 'JSON',
             'size': size,
             'pretty': 'true',
-            'expand' : self.expand,
+            'expand': self.expand,
             'from': start_from
         }
 
         return params
+
+    def get_filename(self, hit):
+        return hit['file_name']
+
+    def get_id(self, hit):
+        return hit['id']
+
+    def get_cases_dict(self, hit):
+        return hit['cases'][0]
+
+    def get_sample_dict(self, hit):
+        return self.get_cases_dict(hit)['samples'][0]
+
+    def get_diagnoses_dict(self, hit):
+        return self.get_cases_dict(hit)['diagnoses'][0]
+
+    def get_tissue_or_organ_of_origin(self, hit):
+        return self.get_diagnoses_dict(hit)['tissue_or_organ_of_origin']
+
+    def get_primary_diagnosis(self, hit):
+        return self.get_diagnoses_dict(hit)['primary_diagnosis']
+
+    def get_tumor_stage(self, hit):
+        return self.get_diagnoses_dict(hit)['tumor_stage']
+
+    def get_demographic_dict(self, hit):
+        return self.get_cases_dict(hit)['demographic']
+
+    def get_gender(self, hit):
+        return self.get_demographic_dict(hit)['gender']
+
+    def get_ethnicity(self, hit):
+        return self.get_demographic_dict(hit)['ethnicity']
+
+    def get_project_dict(self, hit):
+        return self.get_cases_dict(hit)['project']
+
+    def get_program_dict(self, hit):
+        return self.get_project_dict(hit)['program']
+
+    def get_program_name(self, hit):
+        return self.get_program_dict(hit)['name']
+
+    def get_dbgap_accession_number(self, hit):
+        return self.get_project_dict(hit)['dbgap_accession_number']
+
+    def get_disease_type(self, hit):
+        return self.get_cases_dict(hit)['disease_type']
+
+    def get_primary_site(self, hit):
+        return self.get_cases_dict(hit)['primary_site']
+
+    def get_tissue_type(self, hit):
+        return self.get_sample_dict(hit)['tissue_type']
+
+    def get_composition(self, hit):
+        return self.get_sample_dict(hit)['composition']
+
+    def get_sample_type(self, hit):
+        return self.get_sample_dict(hit)['sample_type']
+
+    def get_sample_id(self, hit):
+        return self.get_sample_dict(hit)['sample_id']
+
+    def json_to_dataframe(self, hits):
+
+        import pandas as pd
+
+        index = ['filename', 'tissue_type', 'primary_diagnosis',
+                 'tumor_stage', 'disease_type', 'gender', 'ethnicity',
+                 'program_name', 'dbgap_accession_number', 'primary_site',
+                 'tissue_type', 'composition', 'sample_type', 'sample_id',
+                 'id']
+
+        for hit in hits:
+            outfields = [self.get_filename(hit), self.get_tissue_type(hit), self.get_primary_diagnosis(hit),
+                         self.get_tumor_stage(hit), self.get_disease_type(hit), self.get_gender(hit),
+                         self.get_ethnicity(hit),
+                         self.get_program_name(hit), self.get_dbgap_accession_number(hit), self.get_primary_site(hit),
+                         self.get_tissue_type(hit), self.get_composition(hit), self.get_sample_type(hit),
+                         self.get_sample_id(hit),
+                         self.get_id(hit)]
+
+            return pd.DataFrame(outfields, index=index)
