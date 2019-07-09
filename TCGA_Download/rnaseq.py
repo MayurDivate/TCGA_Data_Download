8fa4df7e4a3d8c54ce87c535149aa48fefd409f9
Mayur Divate <mayur.divate91@gmail.com>
2019-07-09 16:12:43 +1000

    rnaseq default expand and fields

--- TCGA_Download/rnaseq.py
+++ TCGA_Download/rnaseq.py
@@ -2,17 +2,25 @@ import json as js
 
 
 class RNAseq:
-    default_fields = ['primary_site', 'sample_ids']
+    default_fields = ['file_name','cases.primary_site', 'cases.disease_type']
+    default_expand = 'cases,cases.samples,cases.project,cases.project.program'
 
-    def __init__(self, fields=None):
+    def __init__(self, fields=None, expand=None):
 
         if fields is None:
             self.fields = self.default_fields
         else:
             self.fields = fields
 
+        if expand is None:
+            self.expand = self.default_expand
+        else:
+            self.expand = expand
+
         self.filters = self.get_filters()
         self.params = self.get_params()
+
+
 
     def get_filters(self):
 
@@ -45,12 +53,14 @@ class RNAseq:
         return ','.join(self.fields)
 
     def get_params(self, start_from=0, size=10):
+
         params = {
             'filters': js.dumps(self.filters),
             'fields': self.get_fields(),
             'format': 'JSON',
             'size': size,
             'pretty': 'true',
+            'expand' : self.expand,
             'from': start_from
         }
 
