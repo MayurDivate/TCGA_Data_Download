9891cb12e0e3a7d8af4f4849bc9bccda0eec74df
Mayur Divate <mayur.divate91@gmail.com>
2019-07-10 10:52:35 +1000

    page by page metadata download with limiting max records per page to 100

--- rnaseq_metadata_for_fpkm_files.py
+++ rnaseq_metadata_for_fpkm_files.py
@@ -21,13 +21,24 @@ rnaseq_obj.filters = filters
 
 # chuncksize = number of records per page
 
-chunksize = 2
+chunksize = 100
 
 rnaseq_query = query.Query(api.get_files_ep(), rnaseq_obj.get_params(size=chunksize))
 
 js_res = rnaseq_query.get_response()
 
-#print(query.get_pagination(js_res))
+print(query.get_pagination(js_res))
 
-rnaseq_obj.json_to_dataframe(query.get_hits(js_res))
+total_pages = query.get_number_pages(js_res)
 
+# page 1
+print('page number: ', query.get_current_page_number(js_res))
+df = rnaseq_obj.json_to_dataframe(query.get_hits(js_res))
+rnaseq_obj.dump_df_to_file(df, True)
+
+# page 2 to page last page
+for page in range(total_pages - 1):
+    js_res = rnaseq_query.get_next_page_response()
+    print('page number: ', query.get_current_page_number(js_res))
+    df = rnaseq_obj.json_to_dataframe(query.get_hits(js_res))
+    rnaseq_obj.dump_df_to_file(df)
