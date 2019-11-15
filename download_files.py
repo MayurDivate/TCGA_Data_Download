f988b03a7e3d9335b793c42d14103c535c2199da
Mayur Divate <mayur.divate91@gmail.com>
2019-11-15 14:13:33 +1000

    download files by names

--- download_files.py
+++ download_files.py
@@ -1,27 +1,9 @@
 import TCGA_Download.download as dw
 
-<<<<<<< HEAD
 files_list = '/Users/n10337547/Projects/download/files.txt'
-
-=======
-
-files_list = '/Users/mayurdivate/QUT/Work/Projects/NBL.txt'
->>>>>>> 0157a68fe667764cdb60d7dff42f73f3f71ccea5
 
 def get_files_names(flist):
     return [file.rstrip() for file in open(files_list, 'r')]
-
-<<<<<<< HEAD
-    with open(files_list, 'r') as f:
-        for line in f:
-            line = line.rstrip()
-            filenames.append(line)
-
-    print("got the file list")
-    return filenames
-=======
->>>>>>> 0157a68fe667764cdb60d7dff42f73f3f71ccea5
-
 
 filenames = get_files_names(files_list)
 
