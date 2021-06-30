e8591c2a6a1aafe38d91d32eb434554a92d2ac8c
MayurDivate <mayur.divate91@gmail.com>
2021-06-30 17:19:07 +1000

    code generalization

--- download_files.py
+++ download_files.py
@@ -1,16 +1,16 @@
-
-
+import os
 import TCGA_Download.download as dw
 
+dirx = '/Users/mayurdivate/QUT/WFH/TCGA_Labelling/TCGA_data_download'
 
-files_list = '/Users/mayurdivate/QUT/Work/Projects/NBL.txt'
+files_list = os.path.join(dirx, 'redownload_files.txt')
 
-def get_files_names(flist):
+def get_files_names(files_list):
     return [file.rstrip() for file in open(files_list, 'r')]
 
 
 filenames = get_files_names(files_list)
 
-output_dir = '/Users/mayurdivate/QUT/Work/Projects/1_CUP/FPKM_txt'
+output_dir = os.path.join(dirx, 'TCGA_re')
 
 dw.download_files_by_file_names(filenames, output_dir)
